947f77765d4d6703a714a2553d2595297dc5c11a
UMAIR <umair90rb@gmail.com>
2021-01-18 11:40:31 +0500

    before windows installation

--- lib/ui/screens/chat/chat_room.dart
+++ lib/ui/screens/chat/chat_room.dart
@@ -30,7 +30,7 @@ class _ChatRoomState extends State<ChatRoom> with TickerProviderStateMixin {
     return ct;
   }
 
-  Widget _buildMessageDisplay() {
+  Widget s_buildMessageDisplay() {
     return ListView.builder(
       itemCount: _chatThreads.length,
       reverse: true,
